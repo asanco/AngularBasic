ce7063f8e5a6c9d37389314f5c570905b0d3c524
asandovaluniandes <a.sandoval1303@uniandes.edu.co>
2015-02-05 10:59:40 -0500

    TallerAngularBasicFecha
    fecha , falta texto abajo

---
+++ public_html/src/modules/user/userController.js
@@ -0,0 +1,48 @@
+(function () {
+	var app = angular.module('userModule', []);
+ 
+	app.controller('userController', ['$scope', function ($scope) {
+			$scope.user = {};
+			$scope.showForm = false;
+			this.showForm = function () {
+				$scope.showForm = true;
+			};
+			this.save = function () {
+				alert("Guardado: " + $scope.user.firstName + " " + $scope.user.lastName);
+			};
+		}]);
+ 
+	app.directive('userForm', [function () {
+			return {
+				restrict: 'E',
+				templateUrl: 'src/modules/user/userTemplates.html'
+			};
+		}]);
+            
+    
+    
+    var nowTemp = new Date();
+var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+ 
+var checkin = $('#dpd1').datepicker({
+  onRender: function(date) {
+    return date.valueOf() < now.valueOf() ? 'disabled' : '';
+  }
+}).on('changeDate', function(ev) {
+  if (ev.date.valueOf() > checkout.date.valueOf()) {
+    var newDate = new Date(ev.date)
+    newDate.setDate(newDate.getDate() + 1);
+    checkout.setValue(newDate);
+  }
+  checkin.hide();
+  $('#dpd2')[0].focus();
+}).data('datepicker');
+var checkout = $('#dpd2').datepicker({
+  onRender: function(date) {
+    return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+  }
+}).on('changeDate', function(ev) {
+  checkout.hide();
+}).data('datepicker');
+            
+})();
